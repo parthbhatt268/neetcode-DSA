f3509bc439865002cc177c01e11d051617b7a913
Parth Bhatt <52027388+parthbhatt268@users.noreply.github.com>
2026-05-31 09:49:17 +0100

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        // 1. Count the frequency of each element
+        const count = new Map();
+        for (const num of nums) {
+            count.set(num, (count.get(num) || 0) + 1);
+        }
+
+        // 2. Create buckets based on frequency
+        const buckets = [...Array(nums.length + 1)].map(() => []);
+        for (const [num, freq] of count.entries()) {
+            buckets[freq].push(num);
+        }
+
+        // 3. Collect the top k frequent elements
+        const result = [];
+        for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+            result.push(...buckets[i]);
+        }
+
+        return result.slice(0, k);
+    }
+}
